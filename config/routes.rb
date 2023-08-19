43dfb66bacbe9f8e8be1ebffc85847631c18cb87
kimiakalantari <kimiakalantari97@gmail.com>
2023-08-18 20:47:33 -0400

    Create initial user login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   get 'about' => 'about#index'
 
   root to: 'products#index'
